e323908c35a2f757981980ec55f263f7135cf79c
ErikCsatlos12 <csatloserik12@yahoo.com>
2025-11-06 13:18:20 +0200

    Commit 2 Tabs in MainWindow, Appcontext

--- Vizsga1/MainWindow.Designer.cs
+++ Vizsga1/MainWindow.Designer.cs
@@ -29,14 +29,141 @@ namespace UMFST.MIP.Bookstore
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabBooks = new System.Windows.Forms.TabPage();
+            this.tabOrders = new System.Windows.Forms.TabPage();
+            this.tabReports = new System.Windows.Forms.TabPage();
+            this.panelTopControls = new System.Windows.Forms.Panel();
+            this.btnResetDatabase = new System.Windows.Forms.Button();
+            this.btnImportData = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControlMain.SuspendLayout();
+            this.tabBooks.SuspendLayout();
+            this.panelTopControls.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabBooks);
+            this.tabControlMain.Controls.Add(this.tabOrders);
+            this.tabControlMain.Controls.Add(this.tabReports);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(800, 450);
+            this.tabControlMain.TabIndex = 0;
+            // 
+            // tabBooks
+            // 
+            this.tabBooks.Controls.Add(this.statusStrip1);
+            this.tabBooks.Location = new System.Drawing.Point(4, 25);
+            this.tabBooks.Name = "tabBooks";
+            this.tabBooks.Padding = new System.Windows.Forms.Padding(3);
+            this.tabBooks.Size = new System.Drawing.Size(792, 421);
+            this.tabBooks.TabIndex = 0;
+            this.tabBooks.Text = "Books";
+            this.tabBooks.UseVisualStyleBackColor = true;
+            // 
+            // tabOrders
+            // 
+            this.tabOrders.Location = new System.Drawing.Point(4, 25);
+            this.tabOrders.Name = "tabOrders";
+            this.tabOrders.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOrders.Size = new System.Drawing.Size(792, 421);
+            this.tabOrders.TabIndex = 1;
+            this.tabOrders.Text = "Orders";
+            this.tabOrders.UseVisualStyleBackColor = true;
+            // 
+            // tabReports
+            // 
+            this.tabReports.Location = new System.Drawing.Point(4, 25);
+            this.tabReports.Name = "tabReports";
+            this.tabReports.Size = new System.Drawing.Size(792, 421);
+            this.tabReports.TabIndex = 2;
+            this.tabReports.Text = "Reports";
+            this.tabReports.UseVisualStyleBackColor = true;
+            // 
+            // panelTopControls
+            // 
+            this.panelTopControls.Controls.Add(this.btnImportData);
+            this.panelTopControls.Controls.Add(this.btnResetDatabase);
+            this.panelTopControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopControls.Location = new System.Drawing.Point(0, 0);
+            this.panelTopControls.Name = "panelTopControls";
+            this.panelTopControls.Size = new System.Drawing.Size(800, 50);
+            this.panelTopControls.TabIndex = 1;
+            // 
+            // btnResetDatabase
+            // 
+            this.btnResetDatabase.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnResetDatabase.Location = new System.Drawing.Point(662, 0);
+            this.btnResetDatabase.Name = "btnResetDatabase";
+            this.btnResetDatabase.Size = new System.Drawing.Size(138, 50);
+            this.btnResetDatabase.TabIndex = 0;
+            this.btnResetDatabase.Text = "Adatbázis Reset";
+            this.btnResetDatabase.UseVisualStyleBackColor = true;
+            this.btnResetDatabase.Click += new System.EventHandler(this.btnResetDatabase_Click);
+            // 
+            // btnImportData
+            // 
+            this.btnImportData.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnImportData.Location = new System.Drawing.Point(0, 0);
+            this.btnImportData.Name = "btnImportData";
+            this.btnImportData.Size = new System.Drawing.Size(198, 50);
+            this.btnImportData.TabIndex = 1;
+            this.btnImportData.Text = "Adatok Importálása (JSON)";
+            this.btnImportData.UseVisualStyleBackColor = true;
+            this.btnImportData.Click += new System.EventHandler(this.btnImportData_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(3, 392);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(786, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(78, 20);
+            this.lblStatus.Text = "Készen áll.";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelTopControls);
+            this.Controls.Add(this.tabControlMain);
+            this.Name = "MainWindow";
+            this.Text = "Bookstore Manager";
+            this.tabControlMain.ResumeLayout(false);
+            this.tabBooks.ResumeLayout(false);
+            this.tabBooks.PerformLayout();
+            this.panelTopControls.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
 
-            // --- JAVÍTVA ---
-            this.Text = "Bookstore Manager"; // Átírva Form1-ről
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabBooks;
+        private System.Windows.Forms.TabPage tabOrders;
+        private System.Windows.Forms.TabPage tabReports;
+        private System.Windows.Forms.Panel panelTopControls;
+        private System.Windows.Forms.Button btnImportData;
+        private System.Windows.Forms.Button btnResetDatabase;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
     }
 }
--- Vizsga1/MainWindow.cs
+++ Vizsga1/MainWindow.cs
@@ -7,14 +7,236 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net.Http;             // JSON letöltéséhez
+using Newtonsoft.Json;             // JSON feldolgozásához
+using UMFST.MIP.Bookstore.Models;  // Az OOP modelljeink eléréséhez
+using System.IO;                   // A hibalog-fájl írásához
+using System.Data.Entity;          // Az adatbázis-műveletekhez
+using UMFST.MIP.Bookstore.Models;
 namespace UMFST.MIP.Bookstore
 {
     public partial class MainWindow : Form
     {
+        // --- VÁLTOZÓK ---
+        // A JSON letöltési linkje a README-ből
+        private const string JsonDataUrl = "https://cdn.shopify.com/s/files/1/0883/3282/8936/files/data_bookstore_final.json?v=1762418524";
+        // A hibalog fájl neve (a README szerint)
+        private const string ErrorLogFile = "invalid_bookstore.txt";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // --- ESEMÉNYKEZELŐK ---
+
+        /// <summary>
+        /// A "RESET" gomb eseménykezelője.
+        /// </summary>
+        private async void btnResetDatabase_Click(object sender, EventArgs e)
+        {
+            var valasz = MessageBox.Show("Biztosan törölni akarja a teljes adatbázist és újraimportálni az adatokat?\nMinden nem mentett változás elvész!",
+                "Adatbázis Reset Megerősítése", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (valasz == DialogResult.No) return;
+
+            // Elindítjuk a teljes Reset és Import folyamatot
+            await ResetAndImportDataAsync();
+        }
+
+        /// <summary>
+        /// Az "IMPORT" gomb eseménykezelője.
+        /// </summary>
+        private async void btnImportData_Click(object sender, EventArgs e)
+        {
+            var valasz = MessageBox.Show("Újra letölti és feldolgozza az adatokat a linkről?\nEz felülírja a meglévő adatokat, ha az adatbázis üres.",
+                "Import Megerősítése", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (valasz == DialogResult.No) return;
+
+            // Elindítjuk a teljes Reset és Import folyamatot
+            // A README szerint az import gombnak is resetelnie kell az adatbázist
+            await ResetAndImportDataAsync();
+        }
+
+        // --- FŐ LOGIKAI METÓDUSOK ---
+
+        /// <summary>
+        /// A teljes adatbázis törlését és újraimportálását vezénylő fő metódus.
+        /// </summary>
+        private async Task ResetAndImportDataAsync()
+        {
+            // Gombok letiltása a folyamat alatt
+            btnImportData.Enabled = false;
+            btnResetDatabase.Enabled = false;
+            lblStatus.Text = "Folyamat elindítva...";
+
+            try
+            {
+                // --- 1. Adatbázis törlése és újrakészítése ---
+                lblStatus.Text = "Adatbázis törlése...";
+                await Task.Run(() =>
+                {
+                    using (var context = new AppContext())
+                    {
+                        // Törli az adatbázist, ha létezik
+                        context.Database.Delete();
+                        // Újra létrehozza az adatbázist a modellek alapján
+                        context.Database.Initialize(true);
+                    }
+                });
+                lblStatus.Text = "Adatbázis sikeresen létrehozva.";
+
+                // Hibalog törlése
+                if (File.Exists(ErrorLogFile))
+                    File.Delete(ErrorLogFile);
+
+                // --- 2. JSON Letöltés ---
+                lblStatus.Text = "JSON letöltése a szerverről...";
+                string jsonString;
+                using (HttpClient client = new HttpClient())
+                {
+                    // A README által megkövetelt letöltés
+                    jsonString = await client.GetStringAsync(JsonDataUrl);
+                }
+
+                if (string.IsNullOrEmpty(jsonString))
+                {
+                    throw new Exception("A letöltött JSON üres.");
+                }
+
+                // --- 3. JSON Feldolgozás (Deserializálás) ---
+                lblStatus.Text = "JSON feldolgozása...";
+                // Newtonsoft.Json használata
+                RootData data = JsonConvert.DeserializeObject<RootData>(jsonString);
+
+                // --- 4. Adatok importálása és validálása ---
+                lblStatus.Text = "Adatok importálása az adatbázisba...";
+                using (var context = new AppContext())
+                using (StreamWriter log = new StreamWriter(ErrorLogFile, append: true)) // Hibalog megnyitása
+                {
+                    await log.WriteLineAsync($"--- Import Napló ({DateTime.Now}) ---");
+
+                    // Szerzők (Authors) importálása (nincs validációs szabály)
+                    if (data.Authors != null)
+                    {
+                        context.Authors.AddRange(data.Authors);
+                    }
+
+                    // Könyvek (Books) importálása VALIDÁLÁSSAL
+                    if (data.Books != null)
+                    {
+                        foreach (var book in data.Books)
+                        {
+                            // A README szerint az érvényteleneket naplózni kell
+                            if (string.IsNullOrEmpty(book.Isbn) || book.Price < 0 || book.Stock < 0)
+                            {
+                                await log.WriteLineAsync($"[HIBA-KÖNYV] Kihagyva: ISBN='{book.Isbn}', Cím='{book.Title}', Ár={book.Price}, Készlet={book.Stock}");
+                            }
+                            else
+                            {
+                                // Csak az érvényes könyveket adjuk hozzá
+                                context.Books.Add(book);
+                            }
+                        }
+                    }
+
+                    // Fizetések (Payments) importálása (nincs validációs szabály)
+                    if (data.Payments != null)
+                    {
+                        context.Payments.AddRange(data.Payments);
+                    }
+
+                    // Rendelések (Orders) és kapcsolódó adatok importálása
+                    if (data.Orders != null)
+                    {
+                        var customers = new Dictionary<string, Customer>();
+
+                        foreach (var order in data.Orders)
+                        {
+                            bool validOrder = true;
+
+                            // --- Customer validálás és hozzáadás ---
+                            if (order.CustomerJson == null || string.IsNullOrEmpty(order.CustomerJson.Id) || !order.CustomerJson.Email.Contains("@"))
+                            {
+                                await log.WriteLineAsync($"[HIBA-CUSTOMER] Kihagyva: Rendelés ID='{order.Id}', Customer hiba.");
+                                validOrder = false;
+                            }
+                            else
+                            {
+                                // Elkerüljük a duplikált ügyfelek hozzáadását
+                                if (!customers.ContainsKey(order.CustomerJson.Id))
+                                {
+                                    customers.Add(order.CustomerJson.Id, order.CustomerJson);
+                                    context.Customers.Add(order.CustomerJson);
+                                }
+                                order.CustomerId = order.CustomerJson.Id; // Kulcs beállítása
+                            }
+
+                            // --- Dátum validálás ---
+                            if (!DateTime.TryParse(order.DateString, out DateTime parsedDate))
+                            {
+                                await log.WriteLineAsync($"[HIBA-DÁTUM] Kihagyva: Rendelés ID='{order.Id}', Dátum='{order.DateString}'");
+                                validOrder = false;
+                            }
+                            else
+                            {
+                                order.Date = parsedDate; // Validált dátum beállítása
+                            }
+
+                            // Ha a rendelés (vagy a vevő) érvénytelen, kihagyjuk
+                            if (!validOrder) continue;
+
+                            // --- Rendelés hozzáadása ---
+                            context.Orders.Add(order);
+
+                            // --- Tételek (OrderItems) validálása ---
+                            if (order.Items != null)
+                            {
+                                foreach (var item in order.Items)
+                                {
+                                    // Ellenőrizzük, hogy a könyv ISBN-je létezik-e egyáltalán
+                                    // (az "UNKNOWN-ISBN" kiszűrésére)
+                                    bool bookExists = context.Books.Local.Any(b => b.Isbn == item.BookIsbn);
+
+                                    if (item.Qty <= 0 || !bookExists)
+                                    {
+                                        await log.WriteLineAsync($"[HIBA-TÉTEL] Kihagyva: Rendelés ID='{order.Id}', ISBN='{item.BookIsbn}', Qty={item.Qty}");
+                                    }
+                                    else
+                                    {
+                                        item.OrderId = order.Id; // Kulcs beállítása
+                                        item.BookIsbn = item.BookIsbn; // Idegen kulcs beállítása
+                                        context.OrderItems.Add(item);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    // --- 5. Mentés az adatbázisba ---
+                    lblStatus.Text = "Változtatások mentése...";
+                    await context.SaveChangesAsync();
+                }
+
+                lblStatus.Text = "Importálás sikeresen befejezve!";
+                MessageBox.Show("Az adatbázis sikeresen törölve és újraimportálva a JSON alapján!", "Import Kész", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // TODO: Itt kell majd frissíteni a füleken lévő adatokat
+                // pl: await LoadBooksAsync();
+            }
+            catch (Exception ex)
+            {
+                // Általános hiba elkapása (pl. letöltés, JSON feldolgozás)
+                lblStatus.Text = "Kritikus hiba történt!";
+                MessageBox.Show($"Hiba történt a folyamat során: {ex.Message}", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Gombok visszakapcsolása
+                btnImportData.Enabled = true;
+                btnResetDatabase.Enabled = true;
+            }
         }
     }
 }
